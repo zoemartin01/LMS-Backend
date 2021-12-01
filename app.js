6ba99730b2fe9382b8dfa092727a212f57df012f
Adrian Keller <adrian@adrian-keller.de>
2021-12-01 02:16:03 +0100

    fixed CORS Allow-Header error

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let app = express();
 //enable CORS for all origins
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
 
