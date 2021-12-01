6f5bd84bc08c1386aa9589d627a3e4adcc2395ec
Adrian Keller <adrian@adrian-keller.de>
2021-12-01 02:15:28 +0100

    fixed authentication endpoints

--- routes/v1.js
+++ routes/v1.js
@@ -6,37 +6,40 @@ const accessTokenSecret = 'V50jPXQVocPUSPHl0yzPJhXZzh32bp';
 const refreshTokenSecret = '3pqOHs7R1TrCgsRKksPp4J3Kfs0l0X';
 let refreshTokens = [];
 
-router.post('/login', (req, res) => {
-  const { username, password } = req.body;
+router.post('/token', (req, res) => {
+  const { email, password } = req.body;
 
   //@todo needs to be replaced with db call
   const users = [
     {
-      username: 'admin',
+      id: 1,
+      email: 'admin',
       password: 'admin',
       role: 'admin'
-    }, {
-      username: 'visitor',
+    },
+    {
+      id: 11,
+      email: 'visitor',
       password: 'visitor',
       role: 'visitor'
     }
   ];
-  const user = users.find(u => { return u.username === username && u.password === password });
+  const user = users.find(u => { return u.email === email && u.password === password });
 
   if (user) {
     // generate an access token
-    const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret, { expiresIn: '20m' });
-    const refreshToken = jwt.sign({ username: user.username, role: user.role }, refreshTokenSecret);
+    const accessToken = jwt.sign({ id: user.id }, accessTokenSecret, { expiresIn: '20m' });
+    const refreshToken = jwt.sign({ id: user.id }, refreshTokenSecret);
 
     refreshTokens.push(refreshToken);
 
-    res.json({accessToken, refreshToken});
+    res.json({accessToken, refreshToken, role: user.role});
   } else {
     res.sendStatus(401);
   }
 });
 
-router.post('/token', (req, res) => {
+router.post('/token/refresh', (req, res) => {
   const { token } = req.body;
 
   if (!token) {
@@ -52,13 +55,13 @@ router.post('/token', (req, res) => {
       return res.sendStatus(403);
     }
 
-    const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret, { expiresIn: '20m' });
+    const accessToken = jwt.sign({ email: user.email, role: user.role }, accessTokenSecret, { expiresIn: '20m' });
 
     res.json({accessToken});
   });
 });
 
-router.post('/logout', (req, res) => {
+router.delete('/token', (req, res) => {
   const { token } = req.body;
   refreshTokens = refreshTokens.filter(t => t !== token);
 
@@ -84,7 +87,7 @@ const authenticateJWT = (req, res, next) => {
   }
 };
 
-router.get('/auth-test', authenticateJWT, (req, res) => {
+router.get('/token/check', authenticateJWT, (req, res) => {
   res.json({
     "authenticated": "true",
   });
