bb912fe43b71c1c2b2ca08c1010f284990ac3a93
Zoe Martin <git@zoemartin.me>
2021-12-06 16:33:45 +0100

    method format change

--- src/routes/user.controller.ts
+++ src/routes/user.controller.ts
@@ -18,29 +18,29 @@ export class UserController {
     this.router.delete(`${this.path}/:id`, this.deleteUser);
   }
 
-  private getAllUsers = async (req: Request, res: Response) => {
+  private async getAllUsers(req: Request, res: Response) {
     const users = await getRepository(User).find();
     res.send(users);
   };
 
-  private getUserById = async (req: Request, res: Response) => {
+  private async getUserById(req: Request, res: Response) {
     const id = req.params.id;
     const user = await getRepository(User).findOne(id);
     res.send(user);
   };
 
-  private createUser = async (req: Request, res: Response) => {
+  private async createUser(req: Request, res: Response) {
     const user = await getRepository(User).save(req.body);
     res.send(user);
   };
 
-  private updateUser = async (req: Request, res: Response) => {
+  private async updateUser(req: Request, res: Response) {
     const id = req.params.id;
     const user = await getRepository(User).update(id, req.body);
     res.send(user);
   };
 
-  private deleteUser = async (req: Request, res: Response) => {
+  private async deleteUser(req: Request, res: Response) {
     const id = req.params.id;
     const user = await getRepository(User).delete(id);
     res.send(user);
