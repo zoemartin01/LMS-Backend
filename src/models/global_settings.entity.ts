41c937f19b2556f895060cf85b704bf85138b1d7
Zoe Martin <git@zoemartin.me>
2021-12-07 16:21:53 +0100

    added setting entity

---
+++ src/models/global_settings.entity.ts
@@ -0,0 +1,29 @@
+import { Column, Entity } from "typeorm";
+
+/**
+ * Global Setting
+ * 
+ * @typedef {Object} GlobalSetting
+ * @class
+ * 
+ * @property {string} key - The key of the setting
+ * @property {string} value - The value of the setting
+ */
+@Entity()
+export class GlobalSetting {
+    /**
+     * The key of the setting in the format "scope.setting_name"
+     * 
+     * @type {string}
+     */
+    @Column({ type: "varchar", length: 255, unique: true })
+    public key: string;
+
+    /**
+     * The value of the setting
+     * 
+     * @type {string}
+     */
+    @Column()
+    public value: string;
+}
