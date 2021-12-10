cea184a5a018d697d26e9651dad7ffdfcc703cd7
Zoe Martin <git@zoemartin.me>
2021-12-10 17:34:46 +0100

    enum docs

--- src/types/enums/timeslot-type.ts
+++ src/types/enums/timeslot-type.ts
@@ -1,5 +1,10 @@
+/**
+ * An enum representing the type of a timeslot.
+ *
+ * @typedef {booked | available | unavailable} TimeslotType
+ */
 export enum TimeSlotType {
-    booked = '1',
-    available = '2',
-    unavailable = '3',
+  booked = '1',
+  available = '2',
+  unavailable = '3',
 }
