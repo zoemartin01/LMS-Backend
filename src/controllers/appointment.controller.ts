d368d8dc2cd62a0ac750de3f76450bce29a6b761
raabs <uoohi@student.kit.edu>
2021-12-11 20:48:54 +0100

    methods + jsdocs

---
+++ src/controllers/appointment.controller.ts
@@ -0,0 +1,112 @@
+import { getRepository } from 'typeorm';
+import { AppointmentTimeslot } from '../models/appointment.timeslot.entity';
+import { Request, Response } from 'express';
+
+/**
+ * Controller for appointment management
+ */
+export class AppointmentController {
+  /**
+   * Get all appointments
+   * @param {Request} req frontend request to get data about all appointments
+   * @param {Response} res backend response with data about all appointments
+   */
+  public static async getAllAppointments(req: Request, res: Response) {
+    const appointments = await getRepository(AppointmentTimeslot).find();
+    res.send(appointments);
+  }
+
+  /**
+   * Get all appointments related to a specific user
+   * @param {Request} req frontend request to get data about all appointments for user
+   * @param {Response} res backend response with data about all appointments for user
+   */
+  public static async getAppointmentsForUser(req: Request, res: Response) {
+    const id = req.params.id;
+    const appointments = await getRepository(AppointmentTimeslot).findOne(id);
+    res.send(appointments);
+  }
+  /**
+   * Get all appointments related to a specific room
+   * @param {Request} req frontend request to get data about all appointments for room
+   * @param {Response} res backend response with data about all appointments for room
+   */
+  public static async getAppointmentsForRoom(req: Request, res: Response) {
+    const id = req.params.id;
+    const appointments = await getRepository(AppointmentTimeslot).findOne(id);
+    res.send(appointments);
+  }
+
+  /**
+   * Get one appointment with an id
+   * @param {Request} req frontend request to get data about one appointment
+   * @param {Response} res backend response with data about one appointment
+   */
+  public static async getAppointment(req: Request, res: Response) {
+    const appointments = await getRepository(AppointmentTimeslot).find();
+    res.send(appointments);
+  }
+
+  /**
+   * Create a new appointment
+   * @param {Request} req frontend request to create a new appointment
+   * @param {Response} res backend response creation of a new appointment
+   */
+  public static async createAppointment(req: Request, res: Response) {
+    const appointment = await getRepository(AppointmentTimeslot).save(req.body);
+    res.send(appointment);
+  }
+
+  /**
+   * Edit thus update appointment
+   * @param {Request} req frontend request to change data about one appointment
+   * @param {Response} res backend response with data change of one appointment
+   */
+  public static async updateAppointment(req: Request, res: Response) {
+    const id = req.params.id;
+    const appointment = await getRepository(AppointmentTimeslot).update(
+      id,
+      req.body
+    );
+    res.send(appointment);
+  }
+
+  /**
+   * Delete one appointment
+   * @param {Request} req frontend request to delete one appointment
+   * @param {Response} res backend response deletion
+   */
+  public static async deleteAppointment(req: Request, res: Response) {
+    const id = req.params.id;
+    const appointment = await getRepository(AppointmentTimeslot).delete(id);
+    res.send(appointment);
+  }
+
+  /**
+   * Change confirmation status of an appointment to accepted
+   * @param {Request} req frontend request to change data about one appointment
+   * @param {Response} res backend response with data change of one appointment
+   */
+  public static async acceptRequest(req: Request, res: Response) {
+    const id = req.params.id;
+    const appointment = await getRepository(AppointmentTimeslot).update(
+      id,
+      req.body
+    );
+    res.send(appointment);
+  }
+
+  /**
+   * Change confirmation status of an appointment to denied
+   * @param {Request} req frontend request to change data about one appointment
+   * @param {Response} res backend response with data change of one appointment
+   */
+  public static async declineRequest(req: Request, res: Response) {
+    const id = req.params.id;
+    const appointment = await getRepository(AppointmentTimeslot).update(
+      id,
+      req.body
+    );
+    res.send(appointment);
+  }
+}
