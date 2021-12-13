24f441b30c0327034c4ea928373ef1664206d81c
Lena <lena.richau@gmail.com>
2021-12-13 16:02:17 +0100

    added request and response params to method doc

--- src/controllers/inventory.controller.ts
+++ src/controllers/inventory.controller.ts
@@ -5,8 +5,8 @@ export class InventoryController {
    * Get the data of all inventory items
    *
    * @route {GET} /inventory
-   * @param req
-   * @param res
+   * @param {Resquest} req frontend request to get data of all inventory items
+   * @param {Response} res backend response with data of all inventory items
    */
   public static async getAllInventoryItems(req: Request, res: Response) {}
 
@@ -15,8 +15,8 @@ export class InventoryController {
    *
    * @route {GET} /inventory/item/:id
    * @routeParam {string} id - The id of the inventory item
-   * @param req
-   * @param res
+   * @param {Request} req frontend request to get data of one inventory item
+   * @param {Response} res backend response with data of one inventory item
    */
   public static async getInventoryItem(req: Request, res: Response) {}
 
@@ -28,8 +28,8 @@ export class InventoryController {
    * @bodyParam {string [Optional]} name - name of the inventory item
    * @bodyParam {string [Optional]} description - description of the inventory item
    * @bodyParam {number [Optional]} quantity - quantity of the inventory item
-   * @param req
-   * @param res
+   * @param {Request} req frontend request to change data of one inventory item
+   * @param {Response} res backend response with (changed) data of inventory item
    */
   public static async updateInventoryItem(req: Request, res: Response) {}
 
@@ -40,8 +40,8 @@ export class InventoryController {
    * @bodyParam {string} name - the name of the inventory item
    * @bodyParam {string} description - description of the inventory item
    * @bodyParam {number [Optional]} quantity - quantity of the inventory item
-   * @param req
-   * @param res
+   * @param {Request} req frontend request to create one new inventory item
+   * @param {Response} res backend response with data of newly created inventory item
    */
   public static async createInventoryItem(req: Request, res: Response) {}
 
@@ -50,8 +50,8 @@ export class InventoryController {
    *
    * @route {DELETE} /inventory/:id
    * @routeParam {string} id - The id of the inventory item
-   * @param req
-   * @param res
+   * @param {Request} req frontend request to delete one inventory item
+   * @param {Response} res backend response deletion
    */
   public static async deleteInventoryItem(req: Request, res: Response) {}
 }
