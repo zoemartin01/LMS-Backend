237091c3ababfbdebcab45ac761af851b6ac1caa
Zoe Martin <git@zoemartin.me>
2021-12-18 19:13:34 +0100

    enum documentation

--- src/types/enums/video-resolution.ts
+++ src/types/enums/video-resolution.ts
@@ -3,6 +3,10 @@
  *
  * @typedef {V720 | V1080 | V1440 | V2160} VideoResolution
  * @kind enum
+ * @enumerators {V720} The video resolution is 1280x720
+ * @enumerators {V1080} The video resolution is 1920x1080
+ * @enumerators {V1440} The video resolution is 2560x1440
+ * @enumerators {V2160} The video resolution is 3840x2160
  */
 export enum VideoResolution {
   V720 = '1280x720',
