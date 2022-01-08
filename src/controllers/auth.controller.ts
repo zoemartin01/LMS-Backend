2081ecfa2bb6ac4cfb67a50025a64a2374eb68f0
Adrian Keller <adrian@adrian-keller.de>
2022-01-08 14:22:59 +0100

    implemented login

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,14 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
 import { getRepository } from 'typeorm';
-import jsonwebtoken from 'jsonwebtoken';
+import jsonwebtoken, { JwtPayload, VerifyErrors } from 'jsonwebtoken';
+const activedirectory = require('activedirectory');
 const bcrypt = require('bcrypt');
+import moment from "moment";
+import environment from "../environment";
 import { User } from '../models/user.entity';
 import { Token } from '../models/token.entity';
 import { TokenType } from '../types/enums/token-type';
 import { UserRole } from '../types/enums/user-role';
-
-const accessTokenSecret = 'V50jPXQVocPUSPHl0yzPJhXZzh32bp';
-const refreshTokenSecret = '3pqOHs7R1TrCgsRKksPp4J3Kfs0l0X';
 
 /**
  * Controller for Authentication
@@ -24,40 +24,187 @@ export class AuthController {
    * @route {POST} /token
    * @bodyParam {string} email - user's email address
    * @bodyParam {string} password - user's password
+   * @bodyParam {boolean} isActiveDirectory - if user should be logged in with active directory
    * @param {Request} req frontend request to login user with their credentials
    * @param {Response} res backend response with authentication and refresh token
    */
-  public static async login(req: Request, res: Response) {
-    const { email, password } = req.body;
+  public static async login(req: Request, res: Response): Promise<void> {
+    const { email, password, isActiveDirectory } = req.body;
 
-    const user: User | undefined = await getRepository(User).findOne({
-      where: { email, password },
+    isActiveDirectory
+      ? await this.loginWithActiveDirectory(email, password, res)
+      : await this.loginWithCredentials(email, password, res);
+  }
+
+  /**
+   * Logs in user with active directory
+   *
+   * @param {string} email user's email address
+   * @param {string} password user's password
+   * @param {Response} res backend response with authentication and refresh token
+   * @private
+   */
+  private static async loginWithActiveDirectory(email: string, password: string, res: Response): Promise<void> {
+    const ad = new activedirectory.ActiveDirectory(environment.activeDirectoryConfig);
+
+    ad.authenticate(email, password, async (err: object, auth: boolean) => {
+      if (err) {
+        res.status(500).json(err);
+        return;
+      }
+
+      if (!auth) {
+        await this.loginCallback(undefined, res);
+        return;
+      }
+
+      const userRepository = getRepository(User);
+      userRepository.findOne({
+        where: { email },
+      }).then(async (user: User|undefined) => {
+        if (user === undefined) {
+          await this.loginCallback(this.createActiveDirectoryUser(email), res);
+        }else if (!user.isActiveDirectory) {
+          res.status(400).json({
+            message: 'Your account ist not linked to active directory, use regular login.',
+          });
+        }
+
+        await this.loginCallback(user, res);
+      });
+    });
+  }
+
+  /**
+   * Creates a new user using active directory
+   *
+   * @param {string} email email of user
+   * @private
+   */
+  private static createActiveDirectoryUser(email: string): User {
+    const ad = new activedirectory.ActiveDirectory(environment.activeDirectoryConfig);
+    const userRepository = getRepository(User);
+
+    return ad.findUser(email, async (err: boolean, adUser: { givenName: string, surname: string } ) => {
+      const user: User = await userRepository.save(userRepository.create({
+        email,
+        firstName: adUser.givenName,
+        lastName: adUser.surname,
+        password: '',
+        emailVerification: true,
+        isActiveDirectory: true,
+      }));
+      return user;
+    });
+  }
+
+  /**
+   * Logs in user with specified credentials
+   *
+   * @param {string} email user's email address
+   * @param {string} password user's password
+   * @param {Response} res backend response with authentication and refresh token
+   * @private
+   */
+  private static async loginWithCredentials(email: string, password: string, res: Response): Promise<void> {
+    const user: User|undefined = await getRepository(User).findOne({
+      where: { email },
     });
 
-    if (!user) {
-      res.status(400).send({
-        message: 'Invalid email or password',
-      });
-    } else {
-      const accessToken = jsonwebtoken.sign(
-        {
-          userId: user.id,
-        },
-        accessTokenSecret,
-        {
-          expiresIn: '20m',
-        }
-      );
-
-      const refreshToken = jsonwebtoken.sign(
-        {
-          userId: user.id,
-        },
-        refreshTokenSecret
-      );
-
-      res.status(201).json({ accessToken, refreshToken, role: user.role });
+    if (user === undefined) {
+      await this.loginCallback(user, res);
+      return;
     }
+
+    if (user.isActiveDirectory) {
+      res.status(400).json({
+        message: 'Your account ist linked to active directory, use active directory login.',
+      });
+    }
+
+    bcrypt.compare(password, user.password, (result: boolean) => {
+      this.loginCallback(
+        result ? user : undefined,
+        res);
+    });
+  }
+
+  /**
+   * Sends response to user based on login result
+   *
+   * @param {User} user logged in user
+   * @param {Response} res backend response with authentication and refresh token
+   * @private
+   */
+  private static async loginCallback(user: User|undefined, res: Response): Promise<void> {
+    if (user === undefined) {
+      res.status(400).json({
+        message: 'Invalid email or password.',
+      });
+      return;
+    }
+
+    if (!user.emailVerification) {
+      res.status(400).json({
+        message: 'Your email is not verified.',
+      });
+      return;
+    }
+
+    if (user.role === UserRole.pending) {
+      res.status(400).json({
+        message: 'An admin needs to accept your account request.',
+      });
+      return;
+    }
+
+    let accessToken: string, refreshToken: string;
+    ({ accessToken, refreshToken } = await this.generateLoginTokens(user));
+
+    res.status(201).json({ accessToken, refreshToken, role: user.role, userId: user.id });
+  }
+
+  /**
+   * Generates a refresh token and an access token for the specified user
+   *
+   * @param {User} user a user
+   * @private
+   */
+  private static async generateLoginTokens(user: User): Promise<{ accessToken: string, refreshToken: string }> {
+    const tokenRepository = getRepository(Token);
+
+    const expiration = moment().add(20, 'minutes').unix();
+
+    const refreshToken = jsonwebtoken.sign(
+      {
+        userId: user.id,
+      },
+      environment.refreshTokenSecret,
+    );
+
+    const refreshTokenModel = await tokenRepository.save(tokenRepository.create({
+      token: refreshToken,
+      user: user,
+      type: TokenType.refreshToken,
+    }));
+
+    const accessToken = jsonwebtoken.sign(
+      {
+        exp: expiration,
+        userId: user.id,
+      },
+      environment.accessTokenSecret,
+    );
+
+    await tokenRepository.save(tokenRepository.create({
+      token: accessToken,
+      user: user,
+      type: TokenType.authenticationToken,
+      refreshToken: refreshTokenModel,
+      expiresAt: new Date(expiration),
+    }));
+
+    return { accessToken, refreshToken };
   }
 
   /**
@@ -91,7 +238,7 @@ export class AuthController {
 
     jsonwebtoken.verify(
       refreshToken,
-      refreshTokenSecret,
+      environment.refreshTokenSecret,
       (err: any, user: any) => {
         if (err) {
           res.sendStatus(401);
@@ -99,7 +246,7 @@ export class AuthController {
 
         const accessToken = jsonwebtoken.sign(
           { email: user.email, role: user.role },
-          accessTokenSecret,
+          environment.accessTokenSecret,
           { expiresIn: '20m' }
         );
 
@@ -115,7 +262,7 @@ export class AuthController {
    * @param {Request} req frontend request to check if current user's token is valid
    * @param {Response} res backend response
    */
-  public static async checkToken(req: Request, res: Response) {
+  public static async checkToken(req: Request, res: Response): Promise<void> {
     res.sendStatus(204);
   }
 
@@ -136,13 +283,13 @@ export class AuthController {
     if (authHeader) {
       const token = authHeader.split(' ')[1];
 
-      jsonwebtoken.verify(token, accessTokenSecret, (err: any) => {
+      jsonwebtoken.verify(token, environment.accessTokenSecret, (err) => {
         if (!err) {
           getRepository(Token)
             .findOne({
               where: { token, type: TokenType.authenticationToken },
             })
-            .then((tokenObject: Token | undefined) => {
+            .then((tokenObject: Token|undefined) => {
               if (tokenObject != undefined) {
                 next();
               }
@@ -158,7 +305,7 @@ export class AuthController {
    *
    * @param {Request} req current http-request
    */
-  public static async getCurrentUser(req: Request): Promise<User | null> {
+  public static getCurrentUser(req: Request): User|null {
     const authHeader = req.headers['authorization'];
 
     if (authHeader) {
@@ -168,7 +315,7 @@ export class AuthController {
         .findOne({
           where: { token, type: TokenType.authenticationToken },
         })
-        .then((tokenObject: Token | undefined) => {
+        .then((tokenObject: Token|undefined) => {
           if (tokenObject != undefined) {
             return tokenObject.user;
           }
@@ -181,11 +328,12 @@ export class AuthController {
    * Checks if current user is admin
    *
    * @param {Request} req current http-request
+   * @private
    */
-  private static async checkAdmin(req: Request): Promise<boolean> {
-    return this.getCurrentUser(req).then((user: User | null) => {
-      return user === null || user.role == UserRole.admin;
-    });
+  private static checkAdmin(req: Request): boolean {
+    const user: User|null = this.getCurrentUser(req);
+
+    return user === null || user.role == UserRole.admin;
   }
 
   /**
@@ -200,15 +348,8 @@ export class AuthController {
     res: Response,
     next: NextFunction
   ) {
-    this.checkAdmin(req).then((isAdmin: boolean) => {
-      if (isAdmin) {
-        next();
-      } else {
-        res.sendStatus(403);
-      }
-    });
+    this.checkAdmin(req)
+      ? next()
+      : res.sendStatus(403);
   }
-
-  //@todo add jwt tokens to db
-  //@todo hash passwords
 }
