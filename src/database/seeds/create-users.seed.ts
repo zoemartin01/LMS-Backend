5514b45983f2c982c74353b035e8a05a13bf3d97
Zoe Martin <git@zoemartin.me>
2022-01-09 17:02:43 +0100

    add recording seeding

--- src/database/seeds/create-users.seed.ts
+++ src/database/seeds/create-users.seed.ts
@@ -2,6 +2,7 @@ import { Connection } from 'typeorm';
 import { Factory, Seeder } from 'typeorm-seeding';
 import { Message } from '../../models/message.entity';
 import { Order } from '../../models/order.entity';
+import { Recording } from '../../models/recording.entity';
 import { User } from '../../models/user.entity';
 
 export default class CreateUsers implements Seeder {
@@ -15,6 +16,9 @@ export default class CreateUsers implements Seeder {
 
           // create orders for each user
           await factory(Order)({ user }).createMany(10);
+
+          // create recordings for each user
+          await factory(Recording)({ user }).createMany(10);
         });
       });
   }
