84f356f74e4964c541256c38d08a997993efc12a
Zoe Martin <git@zoemartin.me>
2022-01-09 15:31:29 +0100

    user seeding

---
+++ src/database/factories/user.factory.ts
@@ -0,0 +1,27 @@
+import { User } from '../../models/user.entity';
+import { define } from 'typeorm-seeding';
+import Faker from 'faker';
+import { UserRole } from '../../types/enums/user-role';
+
+define(User, (faker: typeof Faker) => {
+  const email = faker.internet.email();
+  const firstName = faker.name.firstName();
+  const lastName = faker.name.lastName();
+  // TODO: hash password
+  const password = faker.internet.password();
+  const role = faker.random.arrayElement([
+    UserRole.pending,
+    UserRole.visitor,
+    UserRole.admin,
+  ]);
+  const emailVerification = faker.datatype.boolean();
+
+  const user = new User();
+  user.email = email;
+  user.firstName = firstName;
+  user.lastName = lastName;
+  user.password = password;
+  user.role = role;
+  user.emailVerification = emailVerification;
+  return user;
+});
