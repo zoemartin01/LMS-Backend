425514e155e8ef63100bbe49a64baed492a9697b
Zoe Martin <git@zoemartin.me>
2022-01-09 16:55:20 +0100

    add message seeding to users because i dont know how to order seeds

--- src/database/seeds/create-users.seed.ts
+++ src/database/seeds/create-users.seed.ts
@@ -1,9 +1,16 @@
 import { Connection } from 'typeorm';
 import { Factory, Seeder } from 'typeorm-seeding';
+import { Message } from '../../models/message.entity';
 import { User } from '../../models/user.entity';
 
 export default class CreateUsers implements Seeder {
   public async run(factory: Factory, connection: Connection): Promise<void> {
-    await factory(User)().createMany(10);
+    await factory(User)()
+      .createMany(10)
+      .then((users) => {
+        users.forEach(async (user) => {
+          await factory(Message)({ recipient: user }).createMany(10);
+        });
+      });
   }
 }
