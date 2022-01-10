1c6b341308ddd26d760d7336ee420a8dbb2f857e
Zoe Martin <git@zoemartin.me>
2022-01-10 16:48:11 +0100

    remove additional stuff from users

--- src/database/seeds/create-users.seed.ts
+++ src/database/seeds/create-users.seed.ts
@@ -7,19 +7,6 @@ import { User } from '../../models/user.entity';
 
 export default class CreateUsers implements Seeder {
   public async run(factory: Factory, connection: Connection): Promise<void> {
-    await factory(User)()
-      .createMany(10)
-      .then((users) => {
-        users.forEach(async (user) => {
-          // create messages for each user
-          await factory(Message)({ recipient: user }).createMany(10);
-
-          // create orders for each user
-          await factory(Order)({ user }).createMany(10);
-
-          // create recordings for each user
-          await factory(Recording)({ user }).createMany(10);
-        });
-      });
+    await factory(User)().createMany(10);
   }
 }
