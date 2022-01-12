014f5e36d0bdb95c9d47085bfe0a17b352a61d04
Lena <lena.richau@gmail.com>
2022-01-12 17:00:07 +0100

    added functionality to method stubs without validation in Inventory Controller

--- src/controllers/inventory.controller.ts
+++ src/controllers/inventory.controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { InventoryItem } from '../models/inventory-item.entity';
 
 /**
  * Controller for inventory management
@@ -14,7 +16,13 @@ export class InventoryController {
    * @param {Request} req frontend request to get data of all inventory items
    * @param {Response} res backend response with data of all inventory items
    */
-  public static async getAllInventoryItems(req: Request, res: Response) {}
+  public static async getAllInventoryItems(req: Request, res: Response) {
+    const inventoryRepository = getRepository(InventoryItem);
+
+    const inventoryItems = await inventoryRepository.find();
+
+    res.json(inventoryItems);
+  }
 
   /**
    * Returns the inventory item data of a specific inventory item
@@ -24,7 +32,23 @@ export class InventoryController {
    * @param {Request} req frontend request to get data of one inventory item
    * @param {Response} res backend response with data of one inventory item
    */
-  public static async getInventoryItem(req: Request, res: Response) {}
+  public static async getInventoryItem(req: Request, res: Response) {
+    const inventoryRepository = getRepository(InventoryItem);
+
+    const inventoryItem: InventoryItem | undefined =
+      await inventoryRepository.findOne({
+        where: { id: req.params.id },
+      });
+
+    if (inventoryItem === undefined) {
+      res.status(404).json({
+        message: 'Inventory item not found.',
+      });
+      return;
+    }
+
+    res.json(inventoryItem);
+  }
 
   /**
    * Creates a new inventory item
@@ -36,7 +60,13 @@ export class InventoryController {
    * @param {Request} req frontend request to create one new inventory item
    * @param {Response} res backend response with data of newly created inventory item
    */
-  public static async createInventoryItem(req: Request, res: Response) {}
+  public static async createInventoryItem(req: Request, res: Response) {
+    const inventoryRepository = getRepository(InventoryItem);
+
+    const inventoryItem = await inventoryRepository.save(req.body);
+
+    return inventoryItem;
+  }
 
   /**
    * Update an inventory item's data
@@ -49,7 +79,25 @@ export class InventoryController {
    * @param {Request} req frontend request to change data of one inventory item
    * @param {Response} res backend response with (changed) data of inventory item
    */
-  public static async updateInventoryItem(req: Request, res: Response) {}
+  public static async updateInventoryItem(req: Request, res: Response) {
+    const inventoryRepository = getRepository(InventoryItem);
+
+    let inventoryItem: InventoryItem | undefined =
+      await inventoryRepository.findOne({
+        where: { id: req.params.id },
+      });
+
+    if (inventoryItem === undefined) {
+      res.status(404).json({
+        message: 'Message not found.',
+      });
+      return;
+    }
+
+    inventoryItem = await inventoryRepository.save(req.body);
+
+    res.json(inventoryItem);
+  }
 
   /**
    * Deletes one inventory item
@@ -59,5 +107,22 @@ export class InventoryController {
    * @param {Request} req frontend request to delete one inventory item
    * @param {Response} res backend response deletion
    */
-  public static async deleteInventoryItem(req: Request, res: Response) {}
+  public static async deleteInventoryItem(req: Request, res: Response) {
+    const inventoryRepository = getRepository(InventoryItem);
+
+    const inventoryItem: InventoryItem | undefined =
+      await inventoryRepository.findOne({
+        where: { id: req.params.id },
+      });
+
+    if (inventoryItem == undefined) {
+      res.status(404).json({
+        message: 'Message not found.',
+      });
+      return;
+    }
+
+    await inventoryRepository.delete(inventoryItem);
+    res.sendStatus(204);
+  }
 }
