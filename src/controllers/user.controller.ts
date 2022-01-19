d740fc8e761b541a4fda119db5b7b9dd3f55ec28
Mario <mario.lehnerer2001@gmail.com>
2022-01-19 11:32:31 +0100

    took care of bad visitors

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -54,8 +54,42 @@ export class UserController {
       });
       return;
     }
-
-    // TODO: check req body for disallowed fields (e.g. role, emailVerification, isActiveDirectory)
+    if (req.body.role !== undefined) {
+      res.status(403).json({
+        message: 'No permission to change role.',
+      });
+      return;
+    }
+    if (req.body.emailVerification !== undefined) {
+      res.status(403).json({
+        message: 'No permission to change email verification.',
+      });
+      return;
+    }
+    if (req.body.isActiveDirectory !== undefined) {
+      res.status(403).json({
+        message: 'No permission to change login option.',
+      });
+      return;
+    }
+    if (req.body.firstName !== undefined || req.body.lastName !== undefined) {
+      res.status(403).json({
+        message: 'No permission to change name.',
+      });
+      return;
+    }
+    if (req.body.email !== undefined) {
+      res.status(403).json({
+        message: 'No permission to change email.',
+      });
+      return;
+    }
+    if (req.body.id !== undefined) {
+      res.status(403).json({
+        message: 'No permission to change id.',
+      });
+      return;
+    }
 
     await userRepository.update({ id: user.id }, req.body).catch((err) => {
       res.status(400).json(err);
@@ -190,7 +224,7 @@ export class UserController {
       return;
     }
 
-    await userRepository.update(user, { emailVerification: true })
+    await userRepository.update(user, { emailVerification: true });
 
     await MessagingController.sendMessageToAllAdmins(
       'Accept User Registration',
