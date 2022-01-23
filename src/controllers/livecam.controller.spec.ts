62b12ff588aa9b59d94ac71f670012e9d4db9423
Zoe Martin <git@zoemartin.me>
2022-01-23 13:43:08 +0100

    add test helpers

--- src/controllers/livecam.controller.spec.ts
+++ src/controllers/livecam.controller.spec.ts
@@ -1,18 +1,28 @@
 import { Connection } from 'typeorm';
-import { runSeeder, useRefreshDatabase, useSeeding } from 'typeorm-seeding';
+import {
+  factory,
+  runSeeder,
+  useRefreshDatabase,
+  useSeeding,
+} from 'typeorm-seeding';
 import App from '../app';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
 import environment from '../environment';
+import { v4 as uuidv4 } from 'uuid';
 import CreateTestUsers from '../database/seeds/create-test-users.seed';
+import { Recording } from '../models/recording.entity';
+import { Helpers } from '../test.spec';
+import { User } from '../models/user.entity';
 
 chai.use(chaiHttp);
 chai.should();
 
 describe('LivecamController', () => {
-  let connection: Connection;
   const app: App = new App(3000);
+  let connection: Connection;
   let auth_header: string;
+  let user: User;
 
   before(async () => {
     process.env.NODE_ENV = 'testing';
@@ -25,13 +35,8 @@ describe('LivecamController', () => {
     await runSeeder(CreateTestUsers);
 
     // Authentication
-    await chai
-      .request(app.app)
-      .post(`${environment.apiRoutes.base}${environment.apiRoutes.auth.login}`)
-      .send({ email: 'admin@test.com', password: 'admin' })
-      .then((res) => {
-        auth_header = `Bearer ${res.body.accessToken}`;
-      });
+    auth_header = await Helpers.getAuthHeader(app);
+    user = await Helpers.getCurrentUser(app);
   });
 
   describe('GET /livecam/recordings', () => {
@@ -67,23 +72,23 @@ describe('LivecamController', () => {
     it('should fail without authentification', (done) => {
       chai
         .request(app.app)
-        .patch(uri.replace(':id', '1'))
+        .patch(uri.replace(':id', uuidv4()))
         .end((err, res) => {
           expect(res.status).to.equal(400);
           done();
         });
     });
 
-    // it('should fail with invalid id', (done) => {
-    //   chai
-    //     .request(app.app)
-    //     .patch(uri.replace(':id', "12345"))
-    //     .set('Authorization', auth_header)
-    //     .end((err, res) => {
-    //       expect(res.status).to.equal(404);
-    //       done();
-    //     });
-    // });
+    it('should fail with invalid id', (done) => {
+      chai
+        .request(app.app)
+        .patch(uri.replace(':id', 'invalid'))
+        .set('Authorization', auth_header)
+        .end((err, res) => {
+          expect(res.status).to.equal(404);
+          done();
+        });
+    });
 
     // it('should fail with negative size', (done) => {
     //   chai
@@ -107,26 +112,17 @@ describe('LivecamController', () => {
     //     });
     // });
 
-    it('should update a specific recording', (done) => {
+    it('should update a specific recording', async () => {
+      const recording = await factory(Recording)(user).create();
+
       chai
         .request(app.app)
-        .get(
-          `${environment.apiRoutes.base}${environment.apiRoutes.livecam.getAllScheduled}`
-        )
+        .patch(uri.replace(':id', recording.id))
         .set('Authorization', auth_header)
+        .send({ size: 1 })
         .end((err, res) => {
-          const id = res.body[0].id;
-
-          chai
-            .request(app.app)
-            .patch(uri.replace(':id', id))
-            .set('Authorization', auth_header)
-            .send({ size: 1 })
-            .end((err, res) => {
-              expect(res.status).to.equal(200);
-              expect(res.body.size).to.be.equal(1);
-              done();
-            });
+          expect(res.status).to.equal(200);
+          expect(res.body.size).to.be.equal(1);
         });
     });
   });
--- src/test.spec.ts
+++ src/test.spec.ts
@@ -1,6 +1,11 @@
-import { Connection } from 'typeorm';
-import { tearDownDatabase, useRefreshDatabase } from 'typeorm-seeding';
+import { Connection, getRepository } from 'typeorm';
+import { useRefreshDatabase } from 'typeorm-seeding';
 import chai from 'chai';
+import environment from './environment';
+import App from './app';
+import { Token } from './models/token.entity';
+import { TokenType } from './types/enums/token-type';
+import { User } from './models/user.entity';
 
 chai.should();
 
@@ -15,7 +20,39 @@ describe('Database', () => {
     connection = await useRefreshDatabase({ connection: 'default' });
   });
 
-  it('should tear down the database', async () => {
-    await tearDownDatabase();
-  });
+  // it('should tear down the database', async () => {
+  //   await tearDownDatabase();
+  // });
 });
+
+export class Helpers {
+  public static async getAuthHeader(app: App): Promise<string> {
+    return await chai
+      .request(app.app)
+      .post(`${environment.apiRoutes.base}${environment.apiRoutes.auth.login}`)
+      .send({ email: 'admin@test.com', password: 'admin' })
+      .then((res) => {
+        return `Bearer ${res.body.accessToken}`;
+      });
+  }
+
+  public static async getCurrentUser(app: App): Promise<User> {
+    const authHeader = await this.getAuthHeader(app);
+
+    if (authHeader === undefined) throw new Error('Auth header is undefined');
+
+    const token = authHeader.split(' ')[1];
+
+    const tokenObject = await getRepository(Token).findOne({
+      where: { token, type: TokenType.authenticationToken },
+    });
+
+    if (tokenObject === undefined) throw new Error('Token is undefined');
+
+    const user = await getRepository(User).findOne(tokenObject.userId);
+
+    if (user === undefined) throw new Error('User is undefined');
+
+    return user;
+  }
+}
