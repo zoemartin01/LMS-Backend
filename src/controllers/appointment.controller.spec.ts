d1987496271dd9b4093cac49f4c388a3005ae9e1
raabs <uoohi@student.kit.edu>
2022-01-26 13:18:56 +0100

    getter test appointments

--- src/controllers/appointment.controller.spec.ts
+++ src/controllers/appointment.controller.spec.ts
@@ -14,6 +14,7 @@ import CreateTestUsers from '../database/seeds/create-test-users.seed';
 import { Helpers } from '../test.spec';
 import { User } from '../models/user.entity';
 import { AppointmentTimeslot } from '../models/appointment.timeslot.entity';
+import { Room } from '../models/room.entity';
 
 chai.use(chaiHttp);
 chai.should();
@@ -25,6 +26,7 @@ describe('RoomController', () => {
   let admin: User;
   let visitorHeader: string;
   let visitor: User;
+  let room: Room;
 
   before(async () => {
     process.env.NODE_ENV = 'testing';
@@ -42,6 +44,7 @@ describe('RoomController', () => {
 
     visitorHeader = await Helpers.getAuthHeader(false);
     visitor = await Helpers.getCurrentUser(visitorHeader);
+    room = await factory(Room)().create();
   });
 
   describe('GET /appointments', () => {
@@ -57,7 +60,7 @@ describe('RoomController', () => {
         });
     });
 
-    it('should get no appointments', (done) => {
+    it('should get initial 20 appointments (seeder)', (done) => {
       // Seeding doesn't create any appointments
       chai
         .request(app.app)
@@ -66,7 +69,7 @@ describe('RoomController', () => {
         .end((err, res) => {
           expect(res.status).to.equal(200);
           expect(res.body).to.be.an('array');
-          expect(res.body.length).to.be.equal(0);
+          expect(res.body.length).to.be.equal(20);
           done();
         });
     });
@@ -75,7 +78,7 @@ describe('RoomController', () => {
     it('should fail as non-admin', (done) => {
       chai
         .request(app.app)
-        .patch(uri)
+        .get(uri)
         .set('Authorization', visitorHeader)
         .end((err, res) => {
           expect(res.status).to.equal(403);
@@ -83,18 +86,20 @@ describe('RoomController', () => {
         });
     });
 
-    it('should get all appointments', async () => {
-      const appointments = await factory(AppointmentTimeslot)().createMany(3);
+    it('should get 3 more appointments', async () => {
+      const appointments = await factory(AppointmentTimeslot)({
+        room,
+        user: admin,
+      }).createMany(3);
 
-      chai
+      const res = await chai
         .request(app.app)
         .get(uri)
-        .set('Authorization', adminHeader)
-        .end((err, res) => {
-          expect(res.status).to.equal(200);
-          expect(res.body).to.be.an('array');
-          expect(res.body.length).to.be.equal(3);
-        });
+        .set('Authorization', adminHeader);
+
+      expect(res.status).to.equal(200);
+      expect(res.body).to.be.an('array');
+      expect(res.body.length).to.be.equal(23);
     });
   });
 
@@ -123,18 +128,155 @@ describe('RoomController', () => {
     });
 
     it('should get a specific appointment', async () => {
-      const appointment = await factory(AppointmentTimeslot)().create();
-      chai
+      const appointment = await factory(AppointmentTimeslot)({
+        room,
+        user: admin,
+      }).create();
+      const res = await chai
         .request(app.app)
         .get(uri.replace(':id', appointment.id))
         .set('Authorization', adminHeader)
-        .send({ size: 1 })
+        .send({ size: 1 });
+
+      expect(res.status).to.equal(200);
+      expect(res.body.id).to.equal(appointment.id);
+    });
+  });
+
+  describe('GET /rooms/:id/appointments', () => {
+    const uri = `${environment.apiRoutes.base}${environment.apiRoutes.appointments.getRoomAppointments}`;
+
+    it('should fail without authentification', (done) => {
+      chai
+        .request(app.app)
+        .get(uri.replace(':id', room.id))
         .end((err, res) => {
-          expect(res.status).to.equal(200);
-          expect(res.body.name).to.exist;
-          expect(res.body.id).to.equal(appointment.id);
+          expect(res.status).to.equal(400);
+          done();
         });
     });
-    //Todo .send??
+
+    it('should fail with invalid id', (done) => {
+      chai
+        .request(app.app)
+        .get(uri.replace(':id', 'invalid'))
+        .set('Authorization', adminHeader)
+        .end((err, res) => {
+          expect(res.status).to.equal(404);
+          done();
+        });
+    });
+
+    it('should get no appointments', (done) => {
+      // Seeding doesn't create any appointments
+      chai
+        .request(app.app)
+        .get(uri.replace(':id', room.id))
+        .set('Authorization', visitorHeader)
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.body.length).to.be.equal(0);
+          done();
+        });
+    });
+
+    it('should get 3 appointments', async () => {
+      const appointments = await factory(AppointmentTimeslot)({
+        room,
+        user: visitor,
+      }).createMany(3);
+
+      const res = await chai
+        .request(app.app)
+        .get(uri.replace(':id', room.id))
+        .set('Authorization', adminHeader);
+
+      expect(res.status).to.equal(200);
+      expect(res.body.length).to.be.equal(3);
+    });
+  });
+
+  describe('GET /appointments/series/:id', () => {
+    const uri = `${environment.apiRoutes.base}${environment.apiRoutes.appointments.getRoomAppointments}`;
+
+    it('should fail without authentification', (done) => {
+      chai
+        .request(app.app)
+        .get(uri.replace(':id', uuidv4()))
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+          done();
+        });
+    });
+
+    it('should fail with invalid id', (done) => {
+      chai
+        .request(app.app)
+        .get(uri.replace(':id', 'invalid'))
+        .set('Authorization', adminHeader)
+        .end((err, res) => {
+          expect(res.status).to.equal(404);
+          done();
+        });
+    });
+
+    it('should get appointments of series', async () => {
+      const seriesId = uuidv4();
+      const appointments = await factory(AppointmentTimeslot)({
+        room,
+        user: visitor,
+        seriesId,
+      }).createMany(3);
+
+      const res = await chai
+        .request(app.app)
+        .get(uri.replace(':id', room.id))
+        .set('Authorization', visitorHeader);
+      expect(res.status).to.equal(200);
+      expect(res.body.length).to.be.equal(3);
+    });
+  });
+
+  describe('GET /user/appointments', () => {
+    const uri = `${environment.apiRoutes.base}${environment.apiRoutes.appointments.getCurrentUserAppointments}`;
+
+    it('should fail without authentification', (done) => {
+      chai
+        .request(app.app)
+        .get(uri)
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+          done();
+        });
+    });
+
+    it('should get initial 10 appointments (seeder)', (done) => {
+      // Seeding doesn't create any appointments
+      chai
+        .request(app.app)
+        .get(uri)
+        .set('Authorization', adminHeader)
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.body).to.be.an('array');
+          expect(res.body.length).to.be.equal(10);
+          done();
+        });
+    });
+
+    it('should get 13 appointments', async () => {
+      const appointments = await factory(AppointmentTimeslot)({
+        room,
+        user: visitor,
+      }).createMany(3);
+
+      const res = await chai
+        .request(app.app)
+        .get(uri)
+        .set('Authorization', visitorHeader);
+
+      expect(res.status).to.equal(200);
+      expect(res.body.length).to.be.equal(13);
+    });
   });
 });
