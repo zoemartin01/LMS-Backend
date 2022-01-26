1bf98377b18365eeddfd2dd47d08969e843f2267
Zoe Martin <git@zoemartin.me>
2022-01-26 18:08:49 +0100

    fix room factory

--- src/database/factories/room.factory.ts
+++ src/database/factories/room.factory.ts
@@ -6,7 +6,7 @@ import { getRepository } from 'typeorm';
 define(Room, (faker: typeof Faker) => {
   const name = faker.random.word();
   const description = faker.lorem.sentence();
-  const maxConcurrentBookings = faker.random.number(3);
+  const maxConcurrentBookings = faker.random.number({ min: 1, max: 3 });
   const autoAcceptBookings = faker.random.boolean();
 
   const room = getRepository(Room).create({
