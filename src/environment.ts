cc20b94fcc12ac49ecbe9936a3f009e89af4f16a
Zoe Martin <git@zoemartin.me>
2022-02-02 17:12:29 +0100

    env vars

--- src/environment.ts
+++ src/environment.ts
@@ -1,27 +1,27 @@
 const environment = {
-  frontendUrl: ' http://localhost:4200',
+  frontendUrl: process.env.HOSTNAME || 'http://localhost:4200',
   pwHashSaltRound: 10,
   accessTokenSecret: 'V50jPXQVocPUSPHl0yzPJhXZzh32bp',
   refreshTokenSecret: '3pqOHs7R1TrCgsRKksPp4J3Kfs0l0X',
   activeDirectoryConfig: {
-    url: 'ldaps://ldap.teco.edu:636',
+    url: process.env.LDAP_URL || 'ldaps://ldap.teco.edu:636',
     //baseDN: 'dc=',
   },
   //@todo add SMTP credentials & sender email
   smtpConfig: {
-    host: 'mail.teco.edu',
-    port: 25,
-    secure: true,
+    host: process.env.SMTP_HOST || 'mail.teco.edu',
+    port: process.env.SMTP_POST || 25,
+    secure: process.env.SMTP_SSL || true,
     auth: {
-      user: 'username',
-      pass: 'password',
+      user: process.env.SMTP_USERNAME || 'username',
+      pass: process.env.SMTP_PASSWORD || 'password',
     },
   },
-  smtpSender: '',
+  smtpSender: process.env.SMTP_SENDER || '',
   livecam_server: {
-    host: 'localhost',
-    port: 7000,
-    apiPath: '/api',
+    host: process.env.LIVECAM_HOST || 'localhost',
+    port: process.env.LIVECAM_PORT || 7000,
+    apiPath: process.env.LIVECAM_PATH || '/api',
     endpoints: {
       list: '/recordings',
       download: '/recordings/:id',
