40afc561add1dd511a8564a92bcfa0b4013efdb5
Zoe Martin <git@zoemartin.me>
2022-02-08 14:00:18 +0100

    fix stuff

--- src/database/helpers.ts
+++ src/database/helpers.ts
@@ -4,13 +4,16 @@ import { getRepository } from 'typeorm';
 import { v4 } from 'uuid';
 import { AvailableTimeslot } from '../models/available.timeslot.entity';
 import { Room } from '../models/room.entity';
+import { UnavailableTimeslot } from '../models/unavaliable.timeslot.entity';
 import { TimeSlotRecurrence } from '../types/enums/timeslot-recurrence';
 
 export async function createTimeslots(room: Room, weeks = 52) {
-  const timeslots: AvailableTimeslot[] = [];
+  const aTimeslots: AvailableTimeslot[] = [];
+  const uTimeslots: UnavailableTimeslot[] = [];
 
   for (let i = 0; i < 5; i++) {
-    const seriesId = v4();
+    const aSeriesId = v4();
+    const uSeriesId = v4();
     const aStart = moment('2022-01-03')
       .add(i, 'days')
       .add(faker.random.number({ min: 4, max: 8 }), 'hours');
@@ -26,29 +29,30 @@ export async function createTimeslots(room: Room, weeks = 52) {
       .add(faker.random.number({ min: 0, max: 3 }), 'hours');
 
     for (let j = 0; j < weeks; j++) {
-      timeslots.push(
+      aTimeslots.push(
         getRepository(AvailableTimeslot).create({
           start: aStart.add(1, 'weeks').toDate(),
           end: aEnd.add(1, 'weeks').toDate(),
           room,
           amount: weeks,
           timeSlotRecurrence: TimeSlotRecurrence.weekly,
-          seriesId,
+          seriesId: aSeriesId,
         })
       );
 
-      timeslots.push(
-        getRepository(AvailableTimeslot).create({
+      uTimeslots.push(
+        getRepository(UnavailableTimeslot).create({
           start: uStart.add(1, 'weeks').toDate(),
           end: uEnd.add(1, 'weeks').toDate(),
           room,
           amount: 52,
           timeSlotRecurrence: TimeSlotRecurrence.weekly,
-          seriesId,
+          seriesId: uSeriesId,
         })
       );
     }
   }
 
-  await getRepository(AvailableTimeslot).save(timeslots);
+  await getRepository(AvailableTimeslot).save(aTimeslots);
+  await getRepository(UnavailableTimeslot).save(uTimeslots);
 }
