8d301918760305c7c9a2c86e1791a1f65ee224c7
Zoe Martin <git@zoemartin.me>
2022-02-09 00:44:39 +0100

    fix seeding?

--- src/database/factories/inventory-item.factory.ts
+++ src/database/factories/inventory-item.factory.ts
@@ -4,8 +4,8 @@ import { InventoryItem } from '../../models/inventory-item.entity';
 import { getRepository } from 'typeorm';
 
 define(InventoryItem, (faker: typeof Faker) => {
-  const name = faker.commerce.productName();
-  const description = faker.commerce.productDescription();
+  const name = faker.commerce.product();
+  const description = faker.commerce.productMaterial();
   const quantity = faker.random.number({ min: 1, max: 250 });
 
   const item = getRepository(InventoryItem).create({
