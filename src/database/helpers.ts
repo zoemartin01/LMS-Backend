65c914ee21e9e0a1918695cd2dc535b9b316f7df
Zoe Martin <git@zoemartin.me>
2022-02-08 13:16:56 +0100

    oops

--- src/database/helpers.ts
+++ src/database/helpers.ts
@@ -1,6 +1,7 @@
 import faker from 'faker';
 import moment from 'moment';
 import { getRepository } from 'typeorm';
+import { v4 } from 'uuid';
 import { AvailableTimeslot } from '../models/available.timeslot.entity';
 import { Room } from '../models/room.entity';
 import { TimeSlotRecurrence } from '../types/enums/timeslot-recurrence';
@@ -9,6 +10,7 @@ export async function createTimeslots(room: Room, weeks = 52) {
   const timeslots: AvailableTimeslot[] = [];
 
   for (let i = 0; i < 5; i++) {
+    const seriesId = v4();
     const aStart = moment('2022-01-03')
       .add(i, 'days')
       .add(faker.random.number({ min: 4, max: 8 }), 'hours');
@@ -31,6 +33,7 @@ export async function createTimeslots(room: Room, weeks = 52) {
           room,
           amount: weeks,
           timeSlotRecurrence: TimeSlotRecurrence.weekly,
+          seriesId,
         })
       );
 
@@ -41,6 +44,7 @@ export async function createTimeslots(room: Room, weeks = 52) {
           room,
           amount: 52,
           timeSlotRecurrence: TimeSlotRecurrence.weekly,
+          seriesId,
         })
       );
     }
