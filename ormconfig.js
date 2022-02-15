bbe5062fda0a81d233738e5156ee565a6b1b3049
Zoe Martin <git@zoemartin.me>
2022-02-15 02:00:31 +0100

    update orm config for tests

--- ormconfig.js
+++ ormconfig.js
@@ -13,6 +13,7 @@ if (process.env.NODE_ENV === "testing") {
     ...settings,
     host: process.env.DB_HOST || "127.0.0.1",
     port: process.env.DB_PORT || 5433,
+    entities: ["src/models/*.ts"],
   };
 } else {
   module.exports = {
